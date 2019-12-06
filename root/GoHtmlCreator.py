32794c73deba19229468a317fd153d8f7ec0764b
Anatolii Davydko <anatoly.davydko@gmail.com>
2019-12-06 13:15:53 +0200

    Overview printing is imlemented

--- root/GoHtmlCreator.py
+++ root/GoHtmlCreator.py
@@ -7,47 +7,104 @@ class GoHtmlCreator:
         pass
 
     def create_file(self, file_path):
-        file = open(file_path, 'r')
+        file = open(file_path, encoding='utf-8', mode='r')
         token_set = json.loads(file.read())
         file.close()
-        file = open(file_path, 'w')
+        file = open(file_path, encoding='utf-8', mode='w')
 
         package_name = None
         import_list = None
-        overview = None
+        overview = ""
         indexes = None
         items = None  # (type, depency, title, code, description)
 
-        for t in token_set:
-            print(t)
+        result_text = ""
 
+        for ptoken in token_set:
+            if ptoken[0] == 'File overview':
+                for line in ptoken[1]:
+                    overview += line + "\n"
+                overview += '\n\n'
+            elif ptoken[0] == 'package':
+                pass
+            elif ptoken[0] == 'imports':
+                pass
+            elif ptoken[0] == 'constant':
+                pass
+            elif ptoken[0] == 'variable':
+                pass
+            elif ptoken[0] == 'const_arr':
+                pass
+            elif ptoken[0] == 'variable_arr':
+                pass
+            elif ptoken[0] == 'const_arr':
+                pass
+            elif ptoken[0] == 'type':
+                pass
+            elif ptoken[0] == 'function':
+                pass
+
+            print(ptoken)
+
+        if overview != "":
+            result_text += self.envelop_overview(overview)
+
+        file.write(self.final_envelop(result_text))
         file.close()
         pass
     #######################
 
     # Local ###############
+    def envelop_overview(self, text):
+        before = \
+            """
+            <div class="alert alert-primary" role="alert"> Overview </div>
+            <div class="alert alert-light" role="alert">
+            """
+        after = \
+            """
+            </div>
+            """
+
+        content = ""
+        for i in text.split("\n\n"):
+            pref = "<p>"
+            post = "</p>"
+            content += pref + i + post
+
+        return before + content + after
+
     def final_envelop(self, text):
-        before =    '<!doctype html>\n' + \
-                    '<html lang="en">\n' + \
-                    '  <head>\n' + \
-                    '    <!-- Required meta tags -->\n' + \
-                    '    <meta charset="utf-8">\n' + \
-                    '    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\n' + \
-                    '\n' + \
-                    '    <!-- Bootstrap CSS -->\n' + \
-                    '    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">\n' + \
-                    '\n' + \
-                    '    <title>Hello, world!</title>\n' + \
-                    '  </head>\n' + \
-                    '  <body>\n' + \
-                    '\n'
-        after =     '\n' + \
-                    '\n' + \
-                    '    <!-- Optional JavaScript -->\n' + \
-                    '    <!-- jQuery first, then Popper.js, then Bootstrap JS -->\n' + \
-                    '    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>\n' + \
-                    '    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>\n' + \
-                    '    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>\n' + \
-                    '  </body>\n' + \
-                    '</html>\n'
+        before = \
+            """
+            <!doctype html>
+            <html lang="en">
+              <head>
+                <!-- Required meta tags -->
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                
+                <!-- Bootstrap CSS -->
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                <style type="text/css">
+                  div.alert {
+                      margin: 15px;
+                  }
+                </style>
+    
+                <title>Hello, world!</title>
+              </head>
+              <body>
+            """
+
+        after = \
+            """
+                <!-- Optional JavaScript -->
+                <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+              </body>
+            </html>
+            """
         return before + text + after
